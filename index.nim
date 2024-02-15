49edbaa88ac78c28ca5bf1524bb5bc55a4a996a0
Kiloneie <primoz.fs@gmail.com>
2024-02-15 18:52:45 +0100

    Added Back to Top button, removed WIP
    Added back to top button template with nbRawHTML for easier navigation on the nimib pages. And removed the current WIP as it is done.

--- index.nim
+++ index.nim
@@ -53,8 +53,67 @@ template nbSection(name:string) =
   nbToc.output.add "1. <a href=\"#" & anchorName & "\">" & name & "</a>\n"
   #If you get an error from the above line, addToc must be ran before any nbSection 
 
+template nbSubSection(name:string) =
+  index.subsection.inc
+
+  let anchorName = name.toLower.replace(" ", "-")
+  nbText "<a name = \"" & anchorName & "\"></a>\n## " & "&nbsp;&nbsp;" & $index.section & "." & $index.subsection & ". "  & name & "\n\n---" #&nbsp; is inline HTML for a single white space(nothing in markdown)
+  # see below, but any number works for a numbered list
+  nbToc.output.add "  - " & $index.section & r"\." & $index.subsection & r"\. " & "<a href=\"#" & anchorName & "\">" & name & "</a>\n"
+  #If you get an error from the above line, addToc must be ran before any nbSection 
+
+template addButtonBackToTop() =
+  nbRawHtml: """
+      <meta name = "viewport" content = "width=device-width, initial-scale = 1">
+      <style>
+      body {} <!-- This is a comment, this needs to be here body {} -->
+
+      #myBtn {
+        display: none;
+        position: fixed;
+        bottom: 20px;
+        right: 30px;
+        z-index: 99;
+        font-size: 18px;
+        border: none;
+        outline: none;
+        background-color: #1A222D;
+        color: white;
+        cursor: pointer;
+        padding: 15px;
+        border-radius: 4px;
+      }
+
+      #myBtn:hover {background-color: #555;}
+      </style>
+      <body>
+
+      <button onclick = "topFunction()" id = "myBtn" title = "Go to top">Top</button>
+
+      <script>
+      // Get the button
+      let mybutton = document.getElementById("myBtn");
+
+      // When the user scrolls down 20px from the top of the document, show the button
+      window.onscroll = function() {scrollFunction()};
+
+      function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          mybutton.style.display = "block";
+        } else {mybutton.style.display = "none";}
+      }
+
+      // When the user clicks on the button, scroll to the top of the document
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+      </script>
+    """
+
 #TABLE OF CONTENTS - MUST BE RUN BEFORE ANY nbSection !!!
 addToc() 
+addButtonBackToTop()
 
 #Do NOT forget to have the .html file OPEN at all times, otherwise 
   #live preview will NOT work! ANY live preview!
